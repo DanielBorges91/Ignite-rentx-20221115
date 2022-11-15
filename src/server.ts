f983d37bca2786458921144280286afb6c6b6373
Daniel Borges <danprog91@gmail.com>
2022-11-15 17:20:42 -0300

    Adicionando o ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from "express"
+import express, { response } from "express";
 
-const app = express()
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World!" });
+});
 
 app.listen(3333, () => {
   return console.log("Server is running!");
-})
+});
