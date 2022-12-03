2cc673476a1a71c49083d75c94491f2f524b14af
Daniel Borges <danprog91@gmail.com>
2022-12-03 19:48:27 -0300

    Adicionando a coluna de avatar

---
+++ src/database/migrations/1670067930474-AlterUserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterUserAddAvatar1670067930474 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "users",
+      new TableColumn({
+        name: "avatar",
+        type: "varchar",
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("users", "avatar");
+  }
+}
