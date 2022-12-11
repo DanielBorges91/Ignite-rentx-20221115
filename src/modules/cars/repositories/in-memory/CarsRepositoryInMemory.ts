93cd181d833fb73682e6e687e8a0a2c6f8542ebc
Daniel Borges <danprog91@gmail.com>
2022-12-11 07:18:06 -0300

    Continuando o caso de uso de carros

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,7 +14,7 @@ class CarsRepository implements ICarsRepository {
     fine_amount,
     name,
     license_plate,
-  }: ICreateCarDTO): Promise<void> {
+  }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
@@ -28,6 +28,14 @@ class CarsRepository implements ICarsRepository {
     });
 
     this.cars.push(car);
+
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = this.cars.find((car) => car.license_plate === license_plate);
+
+    return car;
   }
 }
 
